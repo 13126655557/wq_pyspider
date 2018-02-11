80548e560b40475cbdbc5ee4811c95ea462bcb5d
wangquan <wangquan@bytedance.com>
2018-02-11 17:42:48 +0800

    add ci build ***plist

---
+++ wq_pypi_demo/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#encoding:UTF-8
+from setuptools import setup
+
+setup(
+    name='wq_setup-demo',
+    version='1.0.0.dev1',
+    description='测试打包上传pip',
+    url='https://github.com/13126655557/wq_pyspider.git',
+    author='wangquan',
+    author_email='13126655557@163.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='wq project python',
+    packages=['wq_random'],
+)
+
---
+++ wq_pypi_demo/wq_random/wq_print_random_num.py
@@ -0,0 +1,7 @@
+#encoding:UTF-8
+import random
+def random_num():
+    return random.randrange(1, 10)
+
+if __name__ == '__main__':
+    pass
